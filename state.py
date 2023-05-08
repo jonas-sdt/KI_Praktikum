1db8e36726e5c7228a777d8f3acf49b2781918e5
Jonas Schmidt <jon-schmidt@outlook.de>
2023-05-08 12:29:30 +0200

    changed state class

--- state.py
+++ state.py
@@ -1,16 +1,45 @@
+import numpy as np
+
 # For now the class still uses the old attributes. Change later, when the new attributes (nxn array) are implemented.
 class State:
-    def __init__(self, position, electrode1_pos, electrode2_pos, agent_area):
-        self.position = position
-        self.electrode1_pos = electrode1_pos
-        self.electrode2_pos = electrode2_pos
-        self.agent_area = agent_area
+    def __init__(self, image, position: tuple, orientation: int, pixel_to_mm_ratio: float):
+        
+        roi_size = 50 # in mm
+        self.matrix = np.zeros((5,5))
+        
+        roi = image[position[0]-roi_size/2*pixel_to_mm_ratio:position[0]+roi_size/2*pixel_to_mm_ratio, position[1]-roi_size/2*pixel_to_mm_ratio:position[1]+roi_size/2*pixel_to_mm_ratio]
+        
+        num_of_pixels_vert = roi.shape[0]
+        num_of_pixels_hor = roi.shape[1]
+        
+        # add wire to matrix from image so that 1 entry in the matrix equals 1cm
+        for row in range(5):
+            for col in range(5):
+
+                # check if area in roi is wire
+                if np.sum(roi[row*num_of_pixels_vert/5:(row+1)*num_of_pixels_vert/5, col*num_of_pixels_hor/5:(col+1)*num_of_pixels_hor/5]) > 0:
+                    self.matrix[row][col] = 1
+
+        if orientation % 180 == 0:
+            self.matrix[0][2] = 2 if self.matrix[0][2] == 0 else 3
+            self.matrix[4][2] = 2 if self.matrix[4][2] == 0 else 3
+        elif orientation % 180 == 45:
+            self.matrix[4][0] = 2 if self.matrix[4][0] == 0 else 3
+            self.matrix[0][4] = 2 if self.matrix[0][4] == 0 else 3
+        elif orientation % 180 == 90:
+            self.matrix[0][2] = 2 if self.matrix[0][2] == 0 else 3
+            self.matrix[4][2] = 2 if self.matrix[4][2] == 0 else 3
+        elif orientation % 180 == 135:
+            self.matrix[0][0] = 2 if self.matrix[0][0] == 0 else 3
+            self.matrix[4][4] == 2 if self.matrix[4][4] == 0 else 3 
+
+    def is_collided(self):
+        return 3 in self.matrix
 
     def __hash__(self):
-        return hash((self.position, self.electrode1_pos, self.electrode2_pos, tuple(map(tuple, self.agent_area))))
+        return hash(self.matrix)
 
     def __eq__(self, other):
         if not isinstance(other, State):
             return False
-        return (self.position, self.electrode1_pos, self.electrode2_pos, tuple(map(tuple, self.agent_area))) == (
-        other.position, other.electrode1_pos, other.electrode2_pos, tuple(map(tuple, other.agent_area)))
+        return self.__hash__() == other.__hash__()
