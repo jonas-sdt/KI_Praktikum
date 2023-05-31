cd8515e02f32f969a710164bc1a718da14c537a3
Iskander97 <128378755+Iskander97@users.noreply.github.com>
2023-05-31 13:46:22 +0200

    Update optimal_policy.py

--- optimal_policy.py
+++ optimal_policy.py
@@ -1,18 +1,17 @@
 import numpy as np
 from action import Action
-from q_value_algorithm import QValueAlgorithm
+from q_learning import QValueAlgorithm
 
 if __name__ == '__main__':
-    q_value_algorithm = QValueAlgorithm()
-    q_value_algorithm.run()
+    q_learning = QValueAlgorithm()
+    q_learning.run()
 
     # Determine optimal policy
     optimal_policy = {}
-    for state, q_values in q_value_algorithm.q_values.items():
+    for state, q_values in q_learning.q_values.items():
         best_action_index = np.argmax(q_values)
-        best_action = q_value_algorithm.action_list[best_action_index]
+        best_action = q_learning.action_list[best_action_index]
         optimal_policy[state] = best_action
-
     print("Optimal Policy:")
     for state, action in optimal_policy.items():
         print(f"State: {state} -> Action: {action}")
