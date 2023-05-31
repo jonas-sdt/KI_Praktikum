9b2f6460cfd86169a24bd6b02e3999e3fa67ccdd
manuel <manuel.boesl@gmail.com>
2023-05-31 15:00:11 +0200

    Added method to show image

--- environment.py
+++ environment.py
@@ -11,6 +11,7 @@ from state import State
 
 class Environment:
     def __init__(self, image: np.array, pixel_to_mm_ratio):
+        cv2.namedWindow("display", cv2.WINDOW_NORMAL)
         self.position = (0, 256)  # TODO: Change to start position
         self.end_position = (512, 256)
         self.orientation = 0
@@ -25,6 +26,19 @@ class Environment:
         self.do_action(Action.RIGHT)
         self.do_action(Action.RIGHT)
 
+    def show_image(self):
+        # Mark the current position of the agent in the image
+        marked_image = self.image.copy()
+        # Convert the image to 8-bit depth
+        marked_image = cv2.normalize(marked_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        # Convert the image to RGB
+        marked_image = cv2.cvtColor(marked_image, cv2.COLOR_GRAY2RGB)
+        marked_image = cv2.circle(marked_image, self.position, radius=0, color=(0, 0, 255), thickness=2)
+        # Show the image
+        cv2.imshow('display', marked_image)
+        # Update the window
+        cv2.waitKey(1)
+
     def do_action(self, action):
         # action is a tuple of (x, y, orientation)
         print("Action: ", action)
@@ -35,9 +49,10 @@ class Environment:
         self.image[self.position[0], self.position[1]] = self.image[self.position[0], self.position[1]] + AGENT
         self.next_state = State(self.image, self.position, self.orientation, self.pixel_to_mm_ratio)
         self.__first_action = False
+        self.show_image()
 
     def reset_agent(self):
-        self.position = (0, 256) # TODO: Change to start position
+        self.position = (0, 256)  # TODO: Change to start position
         self.orientation = 0
         self.image[self.position[0], self.position[1]] = AGENT
         self.do_action(Action.RIGHT)
