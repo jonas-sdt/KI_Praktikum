520d093102cadd5f19f62dedafd0b0fca3147c86
manuel <manuel.boesl@gmail.com>
2023-06-07 22:23:13 +0200

    Added extra options to dialogue window

--- dialogue.py
+++ dialogue.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+from action import Options
 
 
 class ChoiceDialog:
@@ -19,32 +20,40 @@ class ChoiceDialog:
         self.option_label = ttk.Label(self.main_frame, text="Select an option:", anchor=tk.CENTER)
         self.option_label.pack(fill=tk.X, pady=10)
 
-        self.option_menu = ttk.OptionMenu(self.main_frame, self.option_var,"Choose", "Execution", "Training", command=self.option_selected)
+        self.option_menu = ttk.OptionMenu(
+            self.main_frame,
+            self.option_var,
+            "Choose",
+            Options.TRAINING_GENERATED.value,
+            Options.TRAINING_REAL.value,
+            Options.EXECUTE_FINAL.value,
+            command=self.option_selected
+        )
         self.option_menu.pack(fill=tk.X)
 
+        self.number_label = ttk.Label(self.main_frame, text="Enter a number:")
+        self.number_entry = ttk.Entry(self.main_frame)
         self.ok_button = ttk.Button(self.main_frame, text="OK", command=self.close_window, state='disabled')
-        self.ok_button.pack(fill=tk.X, pady=10)
 
         self.choice = None
-        self.chose_execution = None
+        self.entered_number = None
 
     def option_selected(self, selected_option):
         self.choice = selected_option
+        if selected_option == Options.TRAINING_GENERATED.value:
+            self.number_label.pack(fill=tk.X, pady=10)
+            self.number_entry.pack(fill=tk.X)
+        else:
+            self.number_label.pack_forget()
+            self.number_entry.pack_forget()
+        self.ok_button.pack(fill=tk.X, pady=10)
         self.ok_button['state'] = 'normal'  # Enable the button
         # Set the color to black
         self.ok_button.configure(style="Black.TButton")
-    def close_window(self):
-        self.root.destroy()  # Close the window
 
-    def run(self):
-        if self.choice == "Execution":
-            print("User chose Execution")
-            self.chose_execution = True
-        elif self.choice == "Training":
-            print("User chose Training")
-            self.chose_execution = False
-        else:
-            raise ValueError("No option selected")
+    def close_window(self):
+        self.entered_number = self.number_entry.get()
+        self.root.destroy()  # Close the window
 
     def show_dialog(self):
         self.root.deiconify()
@@ -59,8 +68,12 @@ def main():
     style.configure("TButton", background="#4CAF50", font=("Helvetica", 10, "bold"))  # Set button styles
     choice_dialog = ChoiceDialog(root)
     choice_dialog.show_dialog()
-    choice_dialog.run()
-    return choice_dialog.chose_execution
+
+    if choice_dialog.choice:
+        message = f"Option: {choice_dialog.choice}\nEntered Number: {choice_dialog.entered_number}"
+        messagebox.showinfo("Decision", message)
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
